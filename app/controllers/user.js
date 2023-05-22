d2be7cb1b22e529a68f7d68d52f5ecefbeb0922b
gautamswami <gautams5974@gmail.com>
2023-05-22 15:09:13 +0530

    updateDpandPost

--- app/controllers/user.js
+++ app/controllers/user.js
@@ -1,7 +1,7 @@
 const UserModel = require("../model/user");
 const UserLocation = require("../model/userlocation");
 const Userpost = require("../model/userposts");
-const cloudinary = require('../routes/cloudinary')
+const cloudinary = require("../routes/cloudinary");
 
 exports.create = async (req, res) => {
   const user = new UserModel({
@@ -37,7 +37,6 @@ exports.login = async (req, res) => {
   }
 };
 exports.updateuser = async (req, res) => {
-  
   try {
     const user = await UserModel.findOneAndUpdate(
       req.body.find,
@@ -103,28 +102,33 @@ exports.addlocation = async (req, res) => {
 exports.getlocation = async (req, res) => {
   try {
     // const location = await UserLocation.find({ usercity: req.body.usercity });
- 
-      const { usercity } = req.body;
-  
-      // Find all user locations in the specified city
-      const userLocations = await UserLocation.find({ usercity: usercity });
-  
-      if (userLocations.length === 0) {
-        return res.status(404).json({ message: 'No users found in the specified city' });
-      }
-  
-      // Get the usernames from the user locations
-      const usernames = userLocations.map(userLocation => userLocation.username);
-  
-      // Find the users based on the usernames
-      const users = await UserModel.find({ username: { $in: usernames } }).select('username dp ');
-  
-      res.json(users);
-    } catch (error) {
-      console.error(error);
-      res.status(500).json({ message: 'Internal server error' });
+
+    const { usercity } = req.body;
+
+    // Find all user locations in the specified city
+    const userLocations = await UserLocation.find({ usercity: usercity });
+
+    if (userLocations.length === 0) {
+      return res
+        .status(404)
+        .json({ message: "No users found in the specified city" });
     }
-   
+
+    // Get the usernames from the user locations
+    const usernames = userLocations.map(
+      (userLocation) => userLocation.username
+    );
+
+    // Find the users based on the usernames
+    const users = await UserModel.find({ username: { $in: usernames } }).select(
+      "username dp "
+    );
+
+    res.json(users);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Internal server error" });
+  }
 };
 exports.followuser = async (req, res) => {
   try {
@@ -276,39 +280,74 @@ exports.imageadd = async (req, res) => {
   }
 };
 
-exports.friendsposts = async(req,res)=>{
+exports.friendsposts = async (req, res) => {
   try {
     const { username } = req.body;
 
     // Find the user based on the username
     const user = await UserModel.findOne({ username });
-    console.log(user)
+    console.log(user);
     if (!user) {
-      return res.status(404).json({ message: 'User not found' });
+      return res.status(404).json({ message: "User not found" });
     }
 
     // Find all posts of the user's friends
-    const friendsPosts = await Userpost.find({ username: { $in: user.following } });
-    console.log(friendsPosts)
+    const friendsPosts = await Userpost.find({
+      username: { $in: user.following },
+    });
+    console.log(friendsPosts);
 
     res.json(friendsPosts);
   } catch (error) {
     console.error(error);
-    res.status(500).json({ message: 'Internal server error' });
+    res.status(500).json({ message: "Internal server error" });
   }
-}
-exports.newpost  = async (req,res)=>{
-  try{
+};
+exports.newpost = async (req, res) => {
+  try {
     const base64Image = req.body.image; // Assuming the image is sent in the request body as 'image' parameter
- 
-    cloudinary.uploader.upload(base64Image, (error, result) => {
-      if (error) {
-        return res.status(500).json({ error: 'Failed to upload image' });
+
+    let uploaded = await cloudinary.uploader.upload(
+      base64Image,
+      (error, result) => {
+        if (error) {
+          return res.status(500).json({ error: "Failed to upload image" });
+        }
+        res.json({ imageUrl: result.secure_url });
       }
-      res.json({ imageUrl: result.secure_url });
+    );
+    console.log(uploaded);
+
+    let userpost = new Userpost({
+      username: req.body.username,
+      post: uploaded.secure_url,
     });
+    await userpost.save();
+  } catch (e) {
+    res.status(500).json({ ERROR: e });
   }
-  catch(e){
-    res.status(500).json({message:e})
+};
+exports.adddp = async (req, res) => {
+  try {
+    const base64Image = req.body.image; // Assuming the image is sent in the request body as 'image' parameter
+
+    let uploaded = await cloudinary.uploader.upload(
+      base64Image,
+      (error, result) => {
+        if (error) {
+          return res.status(500).json({ error: "Failed to upload image" });
+        }
+        res.json({ imageUrl: result.secure_url });
+      }
+    );
+    await UserModel.findOneAndUpdate(
+      {
+        username: req.body.username,
+      },
+      { dp: uploaded.secure_url },
+      { new: true }
+    );
+  } catch (e) {
+    res.status(500).json({ ERROR: e });
   }
-}
+};
--- app/routes/user.js
+++ app/routes/user.js
@@ -2,33 +2,8 @@ const express = require("express");
 const UserController = require("../controllers/user");
 const MessageController = require("../controllers/message");
 const router = express.Router();
-const multer = require("multer");
-const path = require("path");
-const UserModel = require("../model/user");
-const Userpost = require("../model/userposts");
-const cloudinary = require("./cloudinary");
 const uploader = require("./multer");
 
-const storage = multer.diskStorage({
-  destination: "./uploads/dp",
-  filename: (req, file, cb) => {
-    return cb(
-      null,
-      `${file.fieldname}_${Date.now()}${path.extname(file.originalname)}`
-    );
-  },
-});
-const poststorage = multer.diskStorage({
-  destination: "./uploads/post",
-  filename: (req, file, cb) => {
-    return cb(
-      null,
-      `${file.fieldname}_${Date.now()}${path.extname(file.originalname)}`
-    );
-  },
-});
-const upload = multer({ storage: storage });
-const uploadpost = multer({ storage: poststorage });
 // login addloc and get city users after that get other user deatils in city and that user detail from that user deails get friends posts
 
 router.post("/signup", UserController.create);
@@ -56,38 +31,9 @@ router.get("/getmessageroom", MessageController.getmessageroom);
 
 //add media routes
 router.post("/image", UserController.imageadd);
-router.post("/adddp", uploader.single("image"), async (req, res) => {
-  const upload = await cloudinary.v2.uploader.upload(req.file.path);
-  res.json({
-    success: true,
-    file: upload.secure_url,
-  });
-  await UserModel.findOneAndUpdate(
-    {
-      username: req.body.username,
-    },
-    { dp: upload.secure_url },
-    { new: true }
-  );
-});
+router.post("/adddp", uploader.single("image"), UserController.adddp);
 
-router.post("/addpost", uploader.single("image"), async (req, res) => {
-  try {
-    const upload = await cloudinary.v2.uploader.upload(req.file.path);
-    res.json({
-      success: true,
-      file: upload.secure_url,
-    });
-    let userpost = new Userpost({
-      username: req.body.username,
-      post: upload.secure_url,
-    });
-    await userpost.save();
-  } catch (e) {
-    res.status(500).json("ERROR");
-  }
-});
-router.post('/newpost',UserController.newpost);
+router.post("/newpost", UserController.newpost);
 router.post("/friendsposts", UserController.friendsposts);
 router.post("/followuser", UserController.followuser);
 router.post("/acceptfollow", UserController.acceptfollow);
